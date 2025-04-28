38e217eba63191fbf077aa81b885548d31756924
Flor Martin <florenciamartin05@gmail.com>
2025-04-28 10:05:22 -0300

    otro cambio

--- src/main/java/com/tallerwebi/infraestructura/RepositorioUsuarioImpl.java
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioUsuarioImpl.java
@@ -45,6 +45,7 @@ public class RepositorioUsuarioImpl implements RepositorioUsuario {
 
     @Override
     public void modificar(Usuario usuario) {
+
         sessionFactory.getCurrentSession().update(usuario);
     }
 
