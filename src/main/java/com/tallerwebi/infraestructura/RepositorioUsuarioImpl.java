7240cef5bb2dba7ab16c022721d7b05b77e34062
Flor Martin <florenciamartin05@gmail.com>
2025-04-28 09:59:50 -0300

    prueba 2

--- src/main/java/com/tallerwebi/infraestructura/RepositorioUsuarioImpl.java
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioUsuarioImpl.java
@@ -23,7 +23,8 @@ public class RepositorioUsuarioImpl implements RepositorioUsuario {
     public Usuario buscarUsuario(String email, String password) {
 
         final Session session = sessionFactory.getCurrentSession();
-        return (Usuario) session.createCriteria(Usuario.class)
+        return (Usuario)
+                session.createCriteria(Usuario.class)
                 .add(Restrictions.eq("email", email))
                 .add(Restrictions.eq("password", password))
                 .uniqueResult();
